5d76b3ea8accab038a5acb6cbb49164c29e470e6
Angelo-abel <zhongfhe@gmail.com>
2021-08-13 19:56:45 +0800

    Update C语言文件操作函数.cpp

--- C语言文件操作函数.cpp
+++ C语言文件操作函数.cpp
@@ -4,3 +4,6 @@ https://blog.csdn.net/yuanren201/article/details/105235288
 
 // 操作文件获取文件内容大小
 https://www.cnblogs.com/fangyan5218/p/10645961.html
+
+// fread函数读取文件内容多少
+http://blog.sina.com.cn/s/blog_664c545f0100swmq.html
